bf588163fbd8123390c3126a839064b6bf54301d
Dottore <86713563+Dottore112@users.noreply.github.com>
2022-01-04 17:42:59 +0100

    New plugin!

---
+++ Cassiefigo/Config.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace Cassiefigo
+
+{
+    public class Config : IConfig
+    {
+        [Description("Enable/Disable the Plugin.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Cassie broadcast")]
+
+        public string Cassiefigo { get; set; } = "Xmas_JingleBells";
+    }
+} 
+    
---
+++ Cassiefigo/Handlers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+
+namespace Cassiefigo
+{
+    public class Handlers
+    {
+        public void OnSpawning(SpawningEventArgs ev)
+        {
+            if (ev.Player.Role == RoleType.ChaosConscript || ev.Player.Role == RoleType.ChaosMarauder || ev.Player.Role == RoleType.ChaosRepressor || ev.Player.Role == RoleType.ChaosRifleman)
+            {
+                Cassie.Message(Plugin.Singleton.Config.Cassiefigo, false, true);
+            }
+        }
+
+
+
+    }
+}
